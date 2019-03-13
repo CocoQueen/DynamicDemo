525052aa04a19ffc95bd59665e614de6b25a16a3
Coco <13522975595@163.com>
2019-03-13 15:11:40 +0800

    组件化第三方SDK版本控制

---
+++ app/src/main/java/com/example/wissdom/dynamicdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.wissdom.dynamicdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ *
+ * @author：Coco
+ * date：2019/3/13
+ * version：1.0
+ * description:组件化第三方SDK版本控制
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
